7efe7cc040dd61771043240711c5ed9e3a978f50
Diogo Lima <srdiogolima@gmail.com>
2018-05-03 10:52:29 -0300

    - Validates uniquiness Tournament name
    - Creating a new validation to Tournament
    - Deleting show link from edit view

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,6 @@
 class Tournament < ApplicationRecord
   validates :name, :description, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
   has_attached_file :image, styles: { medium: "240x320#", thumb: "150x150#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
