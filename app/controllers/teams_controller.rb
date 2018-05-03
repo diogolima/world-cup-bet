e5a400b9872052289867d3aa2d5fe084e197e3fc
Diogo Lima <srdiogolima@gmail.com>
2018-05-03 10:51:37 -0300

    - Creating Team model/views/controller
    Creating:
    - Model Team
    - Views
    - Controller
    - Routes

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,54 @@
+class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @teams = Team.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @team.update(team_params)
+        format.html { redirect_to @team, notice: 'Team was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    respond_to do |format|
+      if @team.save
+        format.html { redirect_to @team, notice: 'Team was successfully created' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    @team.destroy
+    respond_to do |format|
+      format.html { redirect_to teams_url, notice: 'Team was successfully destroyed.' }
+    end
+  end
+
+  private
+  def team_params
+    params.require(:team).permit(:name, :image)
+  end
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+end
---
+++ app/models/team.rb
@@ -0,0 +1,5 @@
+class Team < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  has_attached_file :image, styles: { medium: "240x320#", thumb: "150x150#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
