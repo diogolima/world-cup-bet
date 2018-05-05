2029a58f8b79c1604fa18773752bf49bb7c4499e
Diogo Lima <srdiogolima@gmail.com>
2018-05-05 11:36:14 -0300

    - Change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,30 +19,30 @@ Tournament.destroy_all
   'Belgium', 'Panama', 'Tunisia', 'England', #Group G
   'Poland', 'Senegal', 'Colombia', 'Japan' #Group H
 ]
-@tournament_name = [[name: 'World Cup 2018 - Russia',
+@tournament_name = {name: 'World Cup 2018 - Russia',
                     description: 'The 2018 FIFA World Cup will be the 21st FIFA World Cup,
                     a quadrennial international football tournament contested by the men\'s
                     national teams of the member associations of FIFA. It is scheduled to
                     take place in Russia from 14 June to 15 July 2018,[2] after the country
                     was awarded the hosting rights on 2 December 2010.'
-                  ],
-                  [  name:'World Cup 2022 - Qatar',
+                  },
+                  {  name:'World Cup 2022 - Qatar',
                     description: 'The 2022 FIFA World Cup is scheduled to be the 22nd edition of the FIFA World Cup,
                         the quadrennial international men\'s football championship contested by the national
                         teams of the member associations of FIFA. It is scheduled to take place in Qatar in 2022.
                         This will be the first World Cup held in Asia since the 2002 tournament in
                         South Korea and Japan. This will also be the first World Cup ever to be held in the Middle East,
                         and in an Arab and a Muslim-majority country.'
-                    ]
-                  ]
+
+                  }
 
 @tournament = ['world_cup_russia', 'world_cup_qatar']
 @tournament_name.each_with_index do |tournament, index|
   image_path = "#{Rails.root}/app/assets/images/#{@tournament[index]}.png"
   image_file = File.new(image_path)
 Tournament.create!(
-    name: tournament.first[:name],
-    description: tournament.first[:description],
+    name: tournament[:name],
+    description: tournament[:description],
     image: ActionDispatch::Http::UploadedFile.new(
       filename: File.basename(image_file),
       tempfile: image_file,
