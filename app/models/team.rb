f172ed0b530ae16d104f031f1913a84d539e7753
Diogo Lima <srdiogolima@gmail.com>
2018-05-05 11:29:12 -0300

    Change association in game/team model
    - Creating home_games and away_games for team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ApplicationRecord
-  has_many :games
+  has_many :home_games, class_name: :Game, foreign_key: :first_team_id
+  has_many :away_games, class_name: :Game, foreign_key: :second_team_id
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   has_attached_file :image, styles: { medium: "240x320#", thumb: "150x150#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
