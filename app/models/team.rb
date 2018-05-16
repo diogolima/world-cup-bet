5ad8675ef72cfb98bfcce2c518c8f98efbecd5c8
Diogo Lima <srdiogolima@gmail.com>
2018-05-15 22:32:21 -0300

    - Changing team index
    - Add div table responsive to team index

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ApplicationRecord
   has_many :home_games, class_name: :Game, foreign_key: :first_team_id
   has_many :away_games, class_name: :Game, foreign_key: :second_team_id
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  has_attached_file :image, styles: { medium: "240x320#", thumb: "150x150#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "320x240#", thumb: "80x110#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def games
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,6 +2,6 @@ class Tournament < ApplicationRecord
   has_many :games
   validates :name, :description, presence: true
   validates :name, uniqueness: { case_sensitive: false }
-  has_attached_file :image, styles: { medium: "240x320#", thumb: "150x150#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "320x240#", thumb: "110x80#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
