eec31380a078122588f05e012fad6b2c956dd5b3
Diogo Lima <srdiogolima@gmail.com>
2018-05-29 10:47:19 -0300

    - Add dependet bets-user
    - When destroy user, destroy all bets from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  has_many :bets
+  has_many :bets, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
