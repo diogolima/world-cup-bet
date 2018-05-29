53803b1562798972676fda6eddb071a09c1fa942
Diogo Lima <srdiogolima@gmail.com>
2018-05-29 10:50:21 -0300

    - Deleting default_options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_options = {from: '2018.football.bet@gmail.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV['ADDRESS'],
