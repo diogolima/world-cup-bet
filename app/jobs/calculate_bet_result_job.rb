d601497a1c77c644a1952cad56157dec35fa38fc
Diogo Lima <srdiogolima@gmail.com>
2018-06-14 10:37:37 -0300

    New rules of bet and calculating bet score

--- app/jobs/calculate_bet_result_job.rb
+++ app/jobs/calculate_bet_result_job.rb
@@ -5,20 +5,33 @@ class CalculateBetResultJob < ApplicationJob
     bets = Bet.where(game_id: game.id)
     bets.each do |bet|
       bet.bet_score = 0
-      if bet.first_team_score == game.score_first_team
-        bet.bet_score = bet.bet_score + 3
-      end
-      if bet.second_team_score == game.score_second_team
-        bet.bet_score = bet.bet_score + 3
-      end
-      # - Entire score correct
-      if bet.bet_score == 6
-        bet.bet_score = 20
+      if bet.first_team_score == game.score_first_team && bet.second_team_score == game.score_second_team
+        bet.bet_score = 25
       # - Got the correct winner or tie + the score of one of the teams
-      elsif (((game.score_first_team > game.score_second_team) && (bet.first_team_score > bet.second_team_score)) || ((game.score_first_team < game.score_second_team) && (bet.first_team_score < bet.second_team_score)) || ((game.score_first_team == game.score_second_team) && (bet.first_team_score == bet.second_team_score)))
-          bet.bet_score = bet.bet_score + 12
+      elsif (game.score_first_team > game.score_second_team && bet.first_team_score > bet.second_team_score)
+        bet.bet_score = calculate_points(game.score_first_team, game.score_second_team, bet.first_team_score, bet.second_team_score)
+      elsif  (game.score_first_team < game.score_second_team && bet.first_team_score < bet.second_team_score)
+        bet.bet_score = calculate_points(game.score_second_team, game.score_first_team, bet.second_team_score, bet.first_team_score)
+      elsif (game.score_first_team == game.score_second_team && bet.first_team_score == bet.second_team_score)
+        bet.bet_score = 18
       end
       bet.save
+    end
+  end
+
+  def calculate_points(game1, game2, bet1, bet2)
+    bet_score = 10
+    #Correct goals of the winner
+    if game1 == bet1
+      bet_score += 5
+    #Correct goals of the loser
+    elsif game2 == bet2
+      bet_score += 2
+    #Correct goal difference
+  elsif (game1 - game2).abs == (bet1 - bet2).abs
+      bet_score += 8
+    else
+      bet_score
     end
   end
 end
